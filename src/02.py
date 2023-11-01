585adc2683e1dc845a2e7b2d96cdaebfa3533d86
skaura <zhujinhu649@gmail.com>
2023-11-01 21:25:38 +0800

    first commit

---
+++ src/02.py
@@ -0,0 +1,20 @@
+import re
+
+text = open(r"dataset\xiaoshuo.txt","r",encoding="utf-8")
+data = text.read() 
+data = data.split("\n")
+data = [lines for lines in data if len(lines)> 0 ]
+data = [lines.strip() for lines in data]
+
+def create_lookup_table(input_data):
+        vcoab = set(input_data)
+        
+        vcoab_to_int = {word: idx for idx, word in enumerate(vcoab)}
+        
+        int_to_vocab = dict(enumerate(vcoab))
+        
+        return vcoab_to_int,int_to_vocab
+    
+x,y = create_lookup_table(data)
+print(x)
+        
